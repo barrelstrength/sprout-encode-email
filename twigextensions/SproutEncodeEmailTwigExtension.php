185143a78f2069d4371b63c43c4652cc99041220
Ben Parizek <ben@barrelstrengthdesign.com>
2015-12-28 10:45:36 -0500

    Updated variable naming and spacing for rot13 encoding
    - Updated $count variable to begin at 1
    - Updated file to be properly formatted
    - Improved naming of variables and removed redundant comments
    - Renamed $encryptedString => $rot13encryptedString
    - Added $uniqueId to avoid duplicate ID values on HTML elements
    - Renamed $count => $countId
    - Renamed $ajax => $ajaxId
    - Added $encodeId to concatenate full ID variable ($uniqueId, $countId, $ajaxId)
    - Renamed $string => $encodedString

--- twigextensions/SproutEncodeEmailTwigExtension.php
+++ twigextensions/SproutEncodeEmailTwigExtension.php
@@ -6,99 +6,101 @@ use Twig_Filter_Method;
 
 class SproutEncodeEmailTwigExtension extends Twig_Extension
 {
-	private $count = 0;
-    public function getName()
-    {
-        return 'Encode Email';
-    }
+	private $count = 1;
 
-    public function getFilters()
-    {
-        // @TODO - explore the markdown class in Yii.
-        // It may also have several filters for this type of use case
-        return array(
-            'encode'    => new Twig_Filter_Method($this, 'encode', array('is_safe' => array('html'))),
-            'rot13'     => new Twig_Filter_Method($this, 'rot13', array('is_safe' => array('html'))),
-            'entities'  => new Twig_Filter_Method($this, 'entities', array('is_safe' => array('html')))
-        );
-    }
+	public function getName()
+	{
+		return 'Encode Email';
+	}
 
-    /**
-     * Encode string using Rot13.  This function does the same as the Rot13 function
-     * and only exists as it may be easier for some folks to remember 'encode'
-     * 
-     * @param  string $string Value to be encoded
-     * @return mixed          Returns Rot13 encoded string
-     */
-    public function encode($string)
-    {
-        return $this->_encodeStringRot13($string);
-    }
+	public function getFilters()
+	{
+		return array(
+			'encode'   => new Twig_Filter_Method($this, 'encode', array('is_safe' => array('html'))),
+			'rot13'    => new Twig_Filter_Method($this, 'rot13', array('is_safe' => array('html'))),
+			'entities' => new Twig_Filter_Method($this, 'entities', array('is_safe' => array('html')))
+		);
+	}
 
-    /**
-     * Encode string using Rot13.
-     * 
-     * @param  string $string Value to be encoded
-     * @return mixed          Returns Rot13 encoded string
-     */
-    public function rot13($string)
-    {
-        return $this->_encodeStringRot13($string);
-    }
+	/**
+	 * Encode string using Rot13.  This function does the same as the Rot13 function
+	 * and only exists as it may be easier for some folks to remember 'encode'
+	 *
+	 * @param  string $string Value to be encoded
+	 * @return mixed          Returns Rot13 encoded string
+	 */
+	public function encode($string)
+	{
+		return $this->_encodeStringRot13($string);
+	}
 
-    /**
-     * Encode string using HTML Entities.
-     * 
-     * @param  string $string Value to be encoded
-     * @return mixed          Returns string encoded as HTML Entities
-     */
-    public function entities($string) 
-    {
-        return $this->_encodeHtmlEntities($string);
-    }
+	/**
+	 * Encode string using Rot13.
+	 *
+	 * @param  string $string Value to be encoded
+	 * @return mixed          Returns Rot13 encoded string
+	 */
+	public function rot13($string)
+	{
+		return $this->_encodeStringRot13($string);
+	}
 
-    /**
-     * Returns a rot13 encrypted string as well as a JavaScript decoder function.
-     * http://snipplr.com/view/6037/
-     * 
-     * @param  string $string Value to be encoded
-     * @return mixed          An encoded string and javascript decoder function
-     */
-    private function _encodeStringRot13($string) {
+	/**
+	 * Encode string using HTML Entities.
+	 *
+	 * @param  string $string Value to be encoded
+	 * @return mixed          Returns string encoded as HTML Entities
+	 */
+	public function entities($string)
+	{
+		return $this->_encodeHtmlEntities($string);
+	}
 
-        // rot13 encryption
-        $encryptedString = str_replace('"','\"',str_rot13($string));
+	/**
+	 * Returns a rot13 encrypted string as well as a JavaScript decoder function.
+	 * http://snipplr.com/view/6037/
+	 *
+	 * @param  string $string Value to be encoded
+	 * @return mixed          An encoded string and javascript decoder function
+	 */
+	private function _encodeStringRot13($string)
+	{
+		$rot13encryptedString = str_replace('"', '\"', str_rot13($string));
 
-		// Add count for multiple encoded strings in a page
-		$count = $this->count++;
+		$uniqueId = uniqid();
+		$countId  = $this->count++;
+		$ajaxId   = (craft()->request->isAjaxRequest()) ? '-ajax' : '';
 
-		//
-		$ajax = (craft()->request->isAjaxRequest()) ? 'ajax-' : '';
+		$encodeId = 'sproutencodeemail-' . $uniqueId . '-' . $countId . $ajaxId;
 
-        // build the string we'll output to the page
-        $string = '
-		<div id="sproutencodeemailRot13Container-' . $ajax . $count . '"></div>
-		<script type="text/javascript">
-			var sproutencodeemailRot13String = "' . $encryptedString . '";
-			var sproutencodeemailRot13 = sproutencodeemailRot13String.replace(/[a-zA-Z]/g, function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});
-			document.getElementById("sproutencodeemailRot13Container-' . $ajax . $count . '").innerHTML = sproutencodeemailRot13;
-        </script>';
-        
-        return $string;   
-    }
+		$encodedString = '
+	<div id="' . $encodeId . '"></div>
+	<script type="text/javascript">
+		var sproutencodeemailRot13String = "' . $rot13encryptedString . '";
+		var sproutencodeemailRot13 = sproutencodeemailRot13String.replace(/[a-zA-Z]/g, function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});
+		document.getElementById("' . $encodeId . '").innerHTML =
+		sproutencodeemailRot13;
+      </script>';
 
-    /**
-     * Returns a string converted to html entities
-     * http://goo.gl/LPhtJ
-     * 
-     * @param  string $string Value to be encoded
-     * @return mixed          Returns a string converted to html entities
-     */
-    private function _encodeHtmlEntities($string)
-    {
-        $string = mb_convert_encoding($string , 'UTF-32', 'UTF-8');
-        $t = unpack("N*", $string);
-        $t = array_map(function($n) { return "&#$n;"; }, $t);
-        return implode("", $t);
-    }
+		return $encodedString;
+	}
+
+	/**
+	 * Returns a string converted to html entities
+	 * http://goo.gl/LPhtJ
+	 *
+	 * @param  string $string Value to be encoded
+	 * @return mixed          Returns a string converted to html entities
+	 */
+	private function _encodeHtmlEntities($string)
+	{
+		$string = mb_convert_encoding($string, 'UTF-32', 'UTF-8');
+		$t      = unpack("N*", $string);
+		$t      = array_map(function ($n)
+		{
+			return "&#$n;";
+		}, $t);
+
+		return implode("", $t);
+	}
 }
